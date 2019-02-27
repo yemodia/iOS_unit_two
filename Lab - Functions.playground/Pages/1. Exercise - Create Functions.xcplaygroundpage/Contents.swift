198ac5e953d1287507ea81022c315e60c65fa5c0
Yerim <yerim@iMac6.local>
2019-02-27 10:00:40 -0500

    During the Functions playground I was abe to return values and tuples. I also sed different parameters and argument labels. And overall  was able to gain ore knowledge on functions in gerneral and how they work.

--- Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -3,12 +3,34 @@
  
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the funtion and observe the printout.
  */
-
-
+func introduceMyself() {
+    print("My name is Sergio Kitchens, and I got the slime")
+}
+introduceMyself()
 /*:
  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = arc4random_uniform(UInt32(5))` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts. Note that the random number generation will not work unless your function is declared after the `import Foundation` statement below. This is because the `arc4random_uniform(_:)` function is declared in the `Foundation` framework.
  */
 import Foundation
+func magicEightBall() {
+    let randomNum = arc4random_uniform(UInt32(5))
+    if randomNum == 0 {
+        print("Probably")
+    } else if randomNum == 1{
+        print("Quite Possible")
+    }
+    else if randomNum == 2 {
+        print("If you think so")
+    }
+    else if randomNum == 3 {
+        print("Most Definetly")
+    }
+    else {
+        print("That's not likely")
+    }
+}
 
 
+magicEightBall()
+magicEightBall()
+magicEightBall()
 //: page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
