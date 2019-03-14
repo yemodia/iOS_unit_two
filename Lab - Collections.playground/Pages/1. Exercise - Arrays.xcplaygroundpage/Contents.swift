94f2cd5193841c43acfbb8c8e1a7073d304f54f4
Yerim <yerim@iMac6.local>
2019-03-14 14:35:43 -0400

    In the Collections playground I explored how to create dirrerent arrays and dictionaries. Along with that, I used the different methods I could call on the arrays and dictionaries.

---
+++ Lab - Collections.playground/Pages/1. Exercise - Arrays.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+/*:
+ ## Exercise - Arrays
+ 
+ Assume you are an event coordinator for a community charity event and are keeping a list of who has registered. Create a variable `registrationList` that will hold strings. It should be empty after initialization.
+ */
+var resgistrationList: [String] = [ ]
+
+/*:
+ Your friend Sara is the first to register for the event. Add her name to `registrationList` using the `append(_:)` method. Print the contents of the collection.
+ */
+resgistrationList.append("Sara")
+print(resgistrationList)
+
+/*:
+ Add four additional names into the array using the `+=` operator. All of the names should be added in one step. Print the contents of the collection.
+ */
+resgistrationList += ["Brian", "Warren", "Bob", "Karen"]
+print(resgistrationList)
+/*:
+ Use the `insert(_:at:)` method to add `Charlie` into the array as the second element. Print the contents of the collection.
+ */
+resgistrationList.insert("Charlie", at: 1)
+print(resgistrationList)
+
+/*:
+ Someone had a conflict and decided to transfer her registration to someone else. Use array subscripting to change the sixth element to `Rebecca`. Print the contents of the collection.
+ */
+resgistrationList [5] = "Rebecca"
+print(resgistrationList)
+/*:
+ Call `removeLast()` on `registrationList`. If done correctly, this should remove `Rebecca` from the collection. Store the result of `removeLast()` into a new constant `deletedItem`, then print `deletedItem`.
+ */
+var deletedItem = resgistrationList.removeLast()
+print(resgistrationList)
+print(deletedItem)
+
+//: page 1 of 4  |  [Next: App Exercise - Activity Challenge](@next)
